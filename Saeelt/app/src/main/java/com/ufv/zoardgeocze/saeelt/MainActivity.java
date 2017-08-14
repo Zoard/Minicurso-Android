96ddad529afe617c933c6ab47e7ed1cae792ee3c
Zoárd Geöcze <zoardag@gmail.com>
2017-08-14 15:18:46 -0300

    Parte 2 - Criando uma Lista de Locais - Criando um método para botão - Pegando texto do Edit Text e adicionando a lista - Verificando log

--- Saeelt/app/src/main/java/com/ufv/zoardgeocze/saeelt/MainActivity.java
+++ Saeelt/app/src/main/java/com/ufv/zoardgeocze/saeelt/MainActivity.java
@@ -2,12 +2,45 @@ package com.ufv.zoardgeocze.saeelt;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Parte 2
+    private List<String> locais = new ArrayList<>();
+    //Parte 2
+    private EditText nomeLocal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Parte 2
+        this.nomeLocal = (EditText) findViewById(R.id.local_nome);
+    }
+
+    //Parte 2
+    public void checkIn(View view) {
+        String local = String.valueOf(this.nomeLocal.getText());
+        if (!local.equals("")) {
+            this.locais.add(local);
+            Toast.makeText(this, local + " adicionado!" ,Toast.LENGTH_SHORT).show();
+
+            this.nomeLocal.setText("");
+
+        } else {
+            Toast.makeText(this, "Nome do Local é obrigatório", Toast.LENGTH_SHORT).show();
+        }
+
+        //Parte 2
+        Log.i("TAMANHO_VETOR_LOCAIS: ", String.valueOf(this.locais.size()));
+
     }
 }
