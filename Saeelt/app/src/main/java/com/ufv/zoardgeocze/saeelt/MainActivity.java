a61cf1946295996a4ecc4d97ba5bec68c9ab7c23
Zoárd Geöcze <zoardag@gmail.com>
2017-08-15 11:34:54 -0300

    Parte 6 - Criando um Spinner no layout - Colocando itens em um Spinner - Selecionando Itens do Spinner

--- Saeelt/app/src/main/java/com/ufv/zoardgeocze/saeelt/MainActivity.java
+++ Saeelt/app/src/main/java/com/ufv/zoardgeocze/saeelt/MainActivity.java
@@ -8,7 +8,10 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import java.io.Serializable;
@@ -21,9 +24,13 @@ public class MainActivity extends AppCompatActivity {
     private List<String> locais = new ArrayList<>();
     //Parte 2
     private EditText nomeLocal;
-
     //Parte 3
     private static final int LISTA = 0;
+    //Parte 6
+    private String[] categorias = {"-","Restaurante","Bar","Cinema","Universidade","Estádio","Parque","outros"};
+    //Parte 6
+    private Spinner categoriaCombo;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +39,26 @@ public class MainActivity extends AppCompatActivity {
 
         //Parte 2
         this.nomeLocal = (EditText) findViewById(R.id.local_nome);
+
+        //Parte 6
+        this.categoriaCombo = (Spinner) findViewById(R.id.categoria_spinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,this.categorias);
+        this.categoriaCombo.setAdapter(adapter);
+
+        this.categoriaCombo.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String categoria = (String) adapterView.getAdapter().getItem(i);
+                if(!categoria.equals("-")) {
+                    Toast.makeText(view.getContext(), "Categoria escolhida é " + categoria, Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
     }
 
     //Parte 3 - Criação de um Menu e Item de Menu
